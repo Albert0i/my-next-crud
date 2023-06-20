d41fc1cc003327e11154fe2e72daaeff7e8339f6
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2023-06-20 12:29:25 +0800

    add more refs

--- src/app/page.js
+++ src/app/page.js
@@ -53,9 +53,29 @@ export default async function Home() {
 }
 
 /*
-   Server Actions: NextJS 13.4's Best New Feature
-   https://youtu.be/czvSZqnpTHs
+   Reference: 
 
-   How to use fetch API in Next.js?
-   https://rapidapi.com/guides/how-to-use-fetch-api-in-next-js
+   1. Server Actions: NextJS 13.4's Best New Feature
+      https://youtu.be/czvSZqnpTHs
+
+   2. Next.js Server Actions in 15 min
+      https://youtu.be/g1dwTNxGmFQ
+
+   3. Build A Simple CRUD API With Next.js 13 App Router (REST API)
+      https://youtu.be/O-NGENb6LNg
+
+   4. NEXTjs Server Actions
+      https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
+
+   5. How to use fetch API in Next.js?
+      https://rapidapi.com/guides/how-to-use-fetch-api-in-next-js
+
+   6, How to enable cors in Nextjs 13 Route Handlers
+      https://github.com/vercel/next.js/discussions/47933
+
+   7. How to Deploy Nextjs Web Application with PM2
+      https://dykraf.com/blog/deploying-nextjs-web-application-with-pm2
+
+   8. The Purloined Letter
+      https://poemuseum.org/the-purloined-letter/
 */
